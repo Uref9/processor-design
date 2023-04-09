cf16b7ae28ad7a7d0d819436ebf91a8c64fefe18
Uref <youbowtou6@gmail.com>
2023-04-10 07:34:25 +0900

    Change var. name

--- alu32.v
+++ alu32.v
@@ -1,27 +1,32 @@
-module alu32(A, B, S, R);
-  input signed [31:0] A, B;
-  input [2:0] S;
-  output signed [31:0] R;
+module alu32(in_1, in_2, ctrl, out_1);
+  // ctrl
+  // 000 : Add
+  // 001 : Sub
+  
+
+  input signed [31:0] in_1, in_2;
+  input [2:0] ctrl;
+  output signed [31:0] out_1;
 
   function [31:0] op;
-    input [2:0] s;  
-    input signed [31:0] a, b;
+    input [2:0] ctrl;  
+    input signed [31:0] in_1, in_2;
 
     begin
-      case(s)
-        3'b000  :op = a + b;
-        3'b001  :op = a - b;
-        3'b010  :op = a & b;
-        3'b011  :op = a | b;
-        3'b100  :op = a ^ b;
-        3'b101  :op = a >>> b;
-        3'b110  :op = a >> b;
-        3'b111  :op = a << b;
+      case(ctrl)
+        3'b000  :op = in_1 + in_2;
+        3'b001  :op = in_1 - in_2;
+        3'b010  :op = in_1 & in_2;
+        3'b011  :op = in_1 | in_2;
+        3'b100  :op = in_1 ^ in_2;
+        3'b101  :op = in_1 >>> in_2;
+        3'b110  :op = in_1 >> in_2;
+        3'b111  :op = in_1 << in_2;
         default :op = 32'bXXXX_XXXX_XXXX_XXXX_XXXX_XXXX_XXXX_XXXX;
       endcase
     end
   endfunction
 
-  assign R = op(S, A, B);
+  assign out_1 = op(ctrl, in_1, in_2);
 
 endmodule
